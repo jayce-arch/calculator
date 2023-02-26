e99eb84b0139ccc0681182d2d50f911718af4f9f
jayce-arch <83500262+jayce-arch@users.noreply.github.com>
2023-02-27 01:06:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function addToInput(val) {
+    var result = document.getElementById("result");
+    if (val === "+" || val === "-" || val === "*" || val === "/") result.value += " " + val + " ";
+    else result.value += val;
+}
+
+function clearInput() {
+	document.getElementById("result").value = "";
+}
+
+function calculate() {
+    var input = document.getElementById("result").value;
+    var result = eval(input);
+    document.getElementById("result").value = result;
+}
